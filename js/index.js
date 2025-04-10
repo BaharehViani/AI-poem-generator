b30653118a4840e31da627b22dc3dd0f5f021083
bahareh <baharehviani@gmail.com>
2025-04-10 20:29:07 +0330

    style updated

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function generatePoem(event) {
     
     let userInstruction = document.querySelector("#prompt").value;
     const apiKey = "4bada4e2ef8cba4745bcdtf450236obd";
-    let context = "You are a poem expert and love to generate poems based on provided themes or instructions,offering a creative and personalized experience. Your mission is to generate poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+    let context = "You are a poem expert and love to generate poems based on provided themes or instructions,offering a creative and personalized experience. Your mission is to generate poem and separate each line with a <br /> but not for the last line. Make sure to follow the user instructions. Do not include a title to the poem.";
     let prompt = `Generate a short poem about ${userInstruction}.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
